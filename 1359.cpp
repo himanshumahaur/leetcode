27ade0ae94f0b93bf549f19bafe6afa3cd384e84
himanshumahaur <himanshumahaur@outlook.com>
2023-09-10 13:38:37 +0530

    added code for 1359.cpp with recursion and memo

--- 1359.cpp
+++ 1359.cpp
@@ -1,19 +1,71 @@
+// int m = 1000000007;
+
+// long long getseq(set<int> order) {
+//     static map<set<int>, int> myhash;
+//     long long ans = 0;
+//     if(order.size()) {
+//         for(auto i:order) {
+//             if((i<0 && !order.contains(-i)) || (i>0)) {
+//                 set<int> temp = order;
+//                 temp.erase(i);
+//                 if(myhash.count(temp)) {
+//                     ans += myhash[temp];
+//                 }
+//                 else {
+//                     ans += getseq(temp);
+//                 }
+//             }
+//             ans = ans % m;
+//         }
+//     }
+//     else {
+//         return 1;
+//     }
+//     myhash[order] = ans; 
+//     return ans;
+// }
+
+// class Solution {
+// public:
+//     int countOrders(int n) {
+//         set<int> order;
+//         for(int i=0; i<n; i++) {
+//             order.insert(i+1);
+//             order.insert(-i-1);
+//         }
+//         int res  = getseq(order);
+//         return res;
+//     }
+// };
+
 int m = 1000000007;
 
-long long getseq(set<int> order) {
-    static map<set<int>, int> myhash;
-    long long ans = 0;
-    if(order.size()) {
-        for(auto i:order) {
-            if((i<0 && !order.contains(-i)) || (i>0)) {
-                set<int> temp = order;
-                temp.erase(i);
-                if(myhash.count(temp)) {
-                    ans += myhash[temp];
-                }
-                else {
-                    ans += getseq(temp);
-                }
+int mult(int a, int b) {
+    int res = 0;
+    if(a<b) {
+        int t = b;
+        b = a;
+        a = t;
+    }
+    for(int i=0; i<b; i++) {
+        res += a;
+        if(res>m) {
+            res = res%m;
+        }
+    }
+    return res;
+}
+
+int getseq(int n) {
+    static unordered_map<int, int> hash;
+    int ans = 0;
+    if(n>2) {
+        for(int i=1; i<=n-1; i++) {
+            if(hash.count(n-2)) {
+                ans += mult(i, hash[n-2]);
+            }
+            else {
+                ans += mult(i, getseq(n-2));
             }
             ans = ans % m;
         }
@@ -21,19 +73,14 @@ long long getseq(set<int> order) {
     else {
         return 1;
     }
-    myhash[order] = ans; 
+    hash[n] = ans;
     return ans;
 }
 
 class Solution {
 public:
     int countOrders(int n) {
-        set<int> order;
-        for(int i=0; i<n; i++) {
-            order.insert(i+1);
-            order.insert(-i-1);
-        }
-        int res  = getseq(order);
-        return res;
+        return getseq(2*n);
+        return 0;
     }
 };
